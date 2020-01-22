36131889b701ea9be324921b041aca5b804e8a4f
laxmikantbpandhare <laxmikantbhaskar.pandhare@sjsu.edu>
2020-01-22 04:57:29 -0800

    added string and numeric problems

---
+++ 02_strings.py
@@ -0,0 +1,5 @@
+message = "We started with pyrhon learning"
+print(message)
+message2 = 'testing strings'
+print(message2)
+
---
+++ 03_numerics.py
@@ -0,0 +1,19 @@
+a = 99
+print(a)
+type(a)
+import sys
+print(sys.maxint)
+b = 9223372036854775807
+print(b)
+type(b)
+c = 9223372036854775808
+print(c)
+type(c)
+d = 1.5
+print(d)
+type(d)
+e = 1 + 2j
+print(e)
+type(e)
+print(e.real)
+print(e.imag)
