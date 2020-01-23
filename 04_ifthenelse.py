7e8d812f73b80d1b748723b4bd778eb382b80a86
laxmikantbpandhare <laxmikantbhaskar.pandhare@sjsu.edu>
2020-01-23 01:06:33 -0800

    added if else

---
+++ 04_ifthenelse.py
@@ -0,0 +1,7 @@
+input = raw_input("testing if then else statement, please enter input string = ")
+
+if len(input) < 10:
+  print("String is Short")
+  print("please enter string with at least 10 characters")
+else:
+ print("String is long")
---
+++ 05_MULTIPLE_IFELSE.py
@@ -0,0 +1,7 @@
+# Equilateral Triangle - All sides are equal.
+# Isosceles Triangle - Two sides havw the same length.
+# Scalene Triangle - Two sides have the same length.
+
+a = int(raw_input("Enter the length of side a = ")
+b = int(raw_input("Enter the length of side b = ")
+c = int(raw_input("Enter the length of side c = ")
