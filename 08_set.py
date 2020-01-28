47111e738c930f69e98586efe11d7107efb2886b
laxmikantbpandhare <laxmikantbhaskar.pandhare@sjsu.edu>
2020-01-28 00:49:51 -0800

    added set changes and list

--- 08_set.py
+++ 08_set.py
@@ -7,4 +7,34 @@ example.add("Set add")
 
 print(example)
 
+print"length of a set =",len(example)
+
+example.remove(100)
+
+print(example)	
+
+
+newexample = set([12, False, 1.99, "India"])
+
+print(newexample)
+print"length of a set =", len(newexample)
+newexample.clear()
+print"length after Clear = ", len(newexample)
+
+odd = set([1, 3, 5, 7, 9])
+even = set([2, 4, 6, 8, 10])
+prime = set([2, 3, 5, 7])
+
+print(odd.union(even))
+print(even.union(odd))
+
+print"odd numbers = ", odd
+print"even numbers = ", even
+
+print(odd.intersection(even))
+print(prime.intersection(even))
+
+print(2 in prime)
+print(5 in odd)
+print(9 not in even)
 
---
+++ 09_list.py
@@ -0,0 +1,10 @@
+# list
+
+example = list()
+
+example = []
+primes = [2, 3, 5, 7, 11, 13, 17] 
+
+print(primes)
+
+
